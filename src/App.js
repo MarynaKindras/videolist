8485d03ce385edee75672c7f609d1c8b18fa3b9e
MarynaKindras <maryna.kindras@gmail.com>
2024-04-22 21:47:43 +0300

    Create VideoList component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { VideoList } from "./components/VideoList";
+
 function App() {
-  return <h1>Hello, world!</h1>;
+  return (
+    <>
+      <h1>Hello, world!</h1>;
+      <VideoList />
+    </>
+  );
 }
 
 export default App;
